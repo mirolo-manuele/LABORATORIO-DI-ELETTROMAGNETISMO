3018d4674e759e2190b75841c21ce8258cdff748
Manuele <manuele.mirolo05@gmail.com>
2025-10-21 19:02:14 +0200

    Stroli ha imparato a usare pyton

--- relazioni/esperienza_2/codici/coefficente_4eta.py
+++ relazioni/esperienza_2/codici/coefficente_4eta.py
@@ -3,23 +3,24 @@ from array import array
 import numpy as np
 import pandas as pd
 
-df = pd.read_excel("Esperienza_2.xlsx", sheet_name="Temp_1") #legge il file excel
+df = pd.read_excel("Esperienza_2.xlsx", sheet_name="Temp_4") #legge il file excel
 df = df.dropna(subset=["qV/kT", "lnCorr(mA)"]) #controlla solo le colonne V e I. Se una di queste contiene un valore mancante, elimina quella riga
 
-X = array('d', df["qV/kT"].to_numpy(dtype=float)) #df è il dataframe, "V" è il nome della colonna da leggere 
+V = array('d', df["qV/kT"].to_numpy(dtype=float)) #df è il dataframe, "V" è il nome della colonna da leggere 
 I = array('d', df["lnCorr(mA)"].to_numpy(dtype=float)) #to_numpy converte la colonna in un array NumPy di tipo float.
-ex = array('d', [0.0]*len(X))
-ey = array('d', [0.0]*len(I))
 
-v_soglia = 30
-v_min=100
+eV = array('d', [0.0]*len(V))
+eI = array('d', [0.0]*len(I))
+
+v_max=25
+v_min=12
 
 
-c = ROOT.TCanvas("c", "Curva voltammetrica", 800, 600)
+c = ROOT.TCanvas("c", "coefficiente eta", 800, 600)
 
-g=ROOT.TGraphErrors(len(X), X, I, ex, ey)
+g=ROOT.TGraphErrors(len(V), V, I, eV, eI)
 
-g.SetTitle("Curva voltammetrica;sqrt(V)[mV];I[mA]") 
+g.SetTitle("coefficiente eta;qV/kT;I") 
 g.SetMarkerStyle(21) 
 g.SetMarkerColor(ROOT.kBlue) 
 g.SetMarkerSize(0.5) 
@@ -28,22 +29,24 @@ g.SetLineWidth(1)
 
 g.Draw("APL")
 
-indici_fit = [i for i in range(len(X)) if X[i] >= v_soglia and X[i]<= v_min]
+indici_fit = [i for i in range(len(V)) if V[i] >= v_min and V[i]<= v_max]
 
-x_fit = array('d', [X[i] for i in indici_fit])
+x_fit = array('d', [V[i] for i in indici_fit])
 y_fit = array('d', [I[i] for i in indici_fit])
 
 g_fit = ROOT.TGraphErrors(len(x_fit), x_fit, y_fit,) 
 
 ROOT.gStyle.SetOptFit(11)#stampa solo parametri fit ed errori
 
-fit_func = ROOT.TF1("fit_func", "pol1",min(x_fit), max(x_fit))
+fit_func = ROOT.TF1("fit_func", "pol1", min(x_fit), max(x_fit))
 
 # Rinomina dei parametri, mettere prima di fit_result (che fa il fit)
-fit_func.SetParName(0, "q") 
-fit_func.SetParName(1, "m")   
+fit_func.SetParName(0, "q")
+fit_func.SetParName(1, "m")
 
-fit_result = g_fit.Fit(fit_func, "S+", "",min(x_fit), max(x_fit))
+# Eseguire il fit e poi calcolare eta usando il parametro di pendenza m
+fit_result = g_fit.Fit(fit_func, "S+", "", min(x_fit), max(x_fit))
+eta = 1.0 / fit_func.GetParameter(1)
 
   
 g_fit.Draw("Q")#mi serve per far comparire la tabella delle statistiche del fit, Q quiet
@@ -52,8 +55,11 @@ fit_func.SetLineWidth(2)
 fit_func.Draw("SAME")
 
 
+print("eta=",eta)
 c.Modified() 
 c.Update()
 c.Draw()
-c.SaveAs("curva_logaritmica_T1.png") 
+
+c.SaveAs("coefficente_eta4.png") 
 input("Premi Invio per chiudere...") 
+
---
+++ relazioni/esperienza_2/codici/coefficente_eta3.py
@@ -0,0 +1,63 @@
+import ROOT
+from array import array
+import numpy as np
+import pandas as pd
+
+df = pd.read_excel("Esperienza_2.xlsx", sheet_name="Temp_3") #legge il file excel
+df = df.dropna(subset=["qV/kT", "lnCorr(mA)"]) #controlla solo le colonne V e I. Se una di queste contiene un valore mancante, elimina quella riga
+
+V = array('d', df["qV/kT"].to_numpy(dtype=float)) #df è il dataframe, "V" è il nome della colonna da leggere 
+I = array('d', df["lnCorr(mA)"].to_numpy(dtype=float)) #to_numpy converte la colonna in un array NumPy di tipo float.
+
+eV = array('d', [0.0]*len(V))
+eI = array('d', [0.0]*len(I))
+
+v_max=330
+v_min=150
+
+
+c = ROOT.TCanvas("c", "coefficiente eta", 800, 600)
+
+g=ROOT.TGraphErrors(len(V), V, I, eV, eI)
+
+g.SetTitle("coefficiente eta;qV/kT;I") 
+g.SetMarkerStyle(21) 
+g.SetMarkerColor(ROOT.kBlue) 
+g.SetMarkerSize(0.5) 
+g.SetLineColor(ROOT.kRed) 
+g.SetLineWidth(1) 
+
+g.Draw("APL")
+
+indici_fit = [i for i in range(len(V)) if V[i] >= v_min and V[i]<= v_max]
+
+x_fit = array('d', [V[i] for i in indici_fit])
+y_fit = array('d', [I[i] for i in indici_fit])
+
+g_fit = ROOT.TGraphErrors(len(x_fit), x_fit, y_fit) 
+
+ROOT.gStyle.SetOptFit(11)#stampa solo parametri fit ed errori
+
+fit_func = ROOT.TF1("fit_func", "pol1", min(x_fit), max(x_fit))
+
+# Rinomina dei parametri, mettere prima di fit_result (che fa il fit)
+fit_func.SetParName(0, "q")
+fit_func.SetParName(1, "m")
+
+# Eseguire il fit e poi calcolare eta usando il parametro di pendenza m
+fit_result = g_fit.Fit(fit_func, "S+", "", min(x_fit), max(x_fit))
+eta = 1.0 / fit_func.GetParameter(1)
+
+  
+g_fit.Draw("Q")#mi serve per far comparire la tabella delle statistiche del fit, Q quiet
+fit_func.SetLineColor(ROOT.kBlack)
+fit_func.SetLineWidth(2)
+fit_func.Draw("SAME")
+
+
+print("eta=",eta)
+c.Modified() 
+c.Update()
+c.Draw()
+c.SaveAs("coefficente_eta3.png") 
+input("Premi Invio per chiudere...") 
