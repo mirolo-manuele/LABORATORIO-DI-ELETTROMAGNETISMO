f9dc04e784af574f3b9389d2e550b65ab438a2d9
Manuele <manuele.mirolo05@gmail.com>
2025-10-27 08:07:09 +0100

    ok

--- relazioni/esperienza_2/codici/V_vs_T.py
+++ relazioni/esperienza_2/codici/V_vs_T.py
@@ -4,16 +4,16 @@ from array import array
 
 df = pd.read_excel("Esperienza_2.xlsx", sheet_name="Sheet1")
 
-mg = ROOT.TMultiGraph()
+mg = ROOT.TMultiGraph() #multigraph mi permette di disegnare più rette in un unico canvas
 colors = [ROOT.kRed, ROOT.kBlue, ROOT.kGreen, ROOT.kMagenta, ROOT.kOrange]
-y_columns = [col for col in df.columns if col.startswith('V')]
-nomilegenda = ["I1=4mA", "I2=5mA", "I3=6mA", "I4=7mA", "I5=8mA"]
+y_columns = [col for col in df.columns if col.startswith('V')] #prova tutte le colonne che cominciano con V
+nomilegenda = ["I1=4mA", "I2=5mA", "I3=6mA", "I4=7mA", "I5=8mA"] #multigraph crea una legenda in automatico, mi serve per dare i nomi diversi
 
-fits = []
+fits = [] #array creati per salvare grafici e fit
 graphs = []  # tieni traccia dei grafici per la legenda
 
-for i, y_col in enumerate(y_columns):
-    valid_data = df[['T', y_col]].dropna()
+for i, y_col in enumerate(y_columns): #y_col diventa il mio numero finale per l'indice grazie ad enumerate
+    valid_data = df[['T', y_col]].dropna() #rimuovi valori NaN
     if len(valid_data) == 0:
         continue
 
@@ -25,7 +25,7 @@ for i, y_col in enumerate(y_columns):
     graph.SetMarkerStyle(20)
     graph.SetMarkerSize(0.6)
     graph.SetLineColor(ROOT.kWhite)
-    graph.SetTitle(nomilegenda[i])
+    graph.SetTitle(nomilegenda[i])#per avere nomi diversi ogni set di punti, così li vedo diversi nella legenda
 
     fit = ROOT.TF1(f"fit_{y_col}", "pol1", 0, max(x) + 20)
     fit.SetLineColor(colors[i % len(colors)])
@@ -53,8 +53,8 @@ for f in fits:
 
 # Crea legenda solo per i punti
 legend = ROOT.TLegend(0.75, 0.75, 0.9, 0.9)
-for g, label in zip(graphs, nomilegenda):
-    legend.AddEntry(g, label, "p")  # "p" = solo marker (nessuna linea del fit)
+for g, label in zip(graphs, nomilegenda): #Itera su ogni grafico (g) nella lista graphs e sulla corrispondente etichetta (label) in nomilegenda
+    legend.AddEntry(g, label, "p")  # "p" = solo marker (nessuna linea del fit), in pratica metto una legenda solo per i punti altrimenti mi fa anche per i fit
 legend.Draw()
 
 c.Update()
